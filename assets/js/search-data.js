9bd4768ac2d5a9dec5a84f6d72fa18ee6ea51dc0
yqzoetan <yq.zoetan@gmail.com>
2025-11-14 03:22:37 +0000

    Deploying to gh-pages from @ yqzoetan/yqzoetan.github.io@d78944b4da8411e1e5ff819bda8b537d43bf6d30 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -398,11 +398,6 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
             },},{id: "news-looking-forward-to-sharing-our-new-scheme-for-probabilistic-computing-in-mrs",
           title: 'Looking forward to sharing our new scheme for probabilistic computing in MRS!',
           description: "",
